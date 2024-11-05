c23675b232f187e13921750de0d567805f8e6b24
ras-24 <siregar.riyando@gmail.com>
2024-11-05 19:16:40 +0800

    Adding Contract

--- contracts/LaptopTokenERC20.sol
+++ contracts/LaptopTokenERC20.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contacts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contacts/access/Ownable.sol";
+
+contract LaptopToken is ERC20, Ownable {
+    constructor() ERC20("Laptop", "LPT") {}
+
+}
