f2c9d6f8980b0cf1fa62896e39f7983aab4a68f7
ras-24 <siregar.riyando@gmail.com>
2024-11-05 19:23:16 +0800

    Adding function mint & fix import url

--- contracts/LaptopTokenERC20.sol
+++ contracts/LaptopTokenERC20.sol
@@ -1,10 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.7.0 <0.9.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contacts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contacts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contracts/access/Ownable.sol";
 
 contract LaptopToken is ERC20, Ownable {
     constructor() ERC20("Laptop", "LPT") {}
 
+    function mint(address _to, uint256 _amount) public {
+        _mint(_to, _amount * (10 ** decimals()));
+    }
 }
