79022a3140bbf1f61059c013ee1de23f56766b10
ras-24 <siregar.riyando@gmail.com>
2024-11-22 16:41:28 +0800

    Edit function mint to msg.sender

--- contracts/LaptopTokenERC20.sol
+++ contracts/LaptopTokenERC20.sol
@@ -7,8 +7,8 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.0.0/contr
 contract LaptopToken is ERC20, Ownable {
     constructor() ERC20("Laptop", "LPT") {}
 
-    function mint(address _to, uint256 _amount) public {
-        _mint(_to, _amount * (10 ** decimals()));
+    function mint(uint256 _amount) public {
+        _mint(msg.sender, _amount * (10 ** decimals()));
     }
 
     function burn(uint256 _amount) public onlyOwner {
